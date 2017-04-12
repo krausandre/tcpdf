9685ca75cd774282b395c8dbc7207c6bfd3ab23e
Daniel Lorenz <daniel.lorenz@extco.de>
2017-04-12 22:19:31 +0200

    [TASK] #2 - make extension compatibile to TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '0.3.2',
+    'version' => '0.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.7.99',
             'php' => '5.6.0'
         ],
         'conflicts' => [],
