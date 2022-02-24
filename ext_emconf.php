50a704f58d708bb359abdb94ba9d852ba81a9740
Andre Kraus <info@andrekraus.com>
2022-02-24 15:33:50 +0100

    [FIX] Establish basic compatibility for TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF['tcpdf'] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '3.0.1',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99'
+            'typo3' => '11.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
